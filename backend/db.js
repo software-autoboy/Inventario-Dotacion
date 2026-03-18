7046118b44b8a8a3edb4d15551ea4a660f0ea35f
Brainer_Eneider <74028013+anonimo772@users.noreply.github.com>
2026-03-18 14:31:37 -0500

    commit 11

--- backend/db.js
+++ backend/db.js
@@ -4,14 +4,15 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  // Prueba con DB_PASS o DB_PASSWORD por si hubo un error al nombrarla
+  password: process.env.DB_PASS || process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME || 'defaultdb',
+  port: parseInt(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   ssl: {
-    rejectUnauthorized: false // Requerido para Aiven y la mayoría de nubes
+    rejectUnauthorized: false
   }
 });
 
